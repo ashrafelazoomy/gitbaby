84b038ad43a820752bc4cff3a1ca6a56c41f1ada
Adham Zahran <adham.zahran@hotmail.com>
2016-02-14 21:05:31 +0200

    repo name is deduced automagically

--- gitbaby.sh
+++ gitbaby.sh
@@ -8,16 +8,15 @@ gitRepos=~/scripts/gitbaby/gitRepos
 
 if [ "$1" = "ls" ]
 then
-	while read i; do
-		echo $i | cut -d ':' -f 2
-	done <$gitRepos
+	cat $gitRepos | cut -d ':' -f 2
 elif [ "$1" = "cd" ]
 then
 	cd "$(cat $gitRepos | grep $2 | cut -d ':' -f 1)"
 elif [ "$1" = "add" ]
 then
 	dir=$(realpath $2)
-	echo "$dir:$3" >> $gitRepos
+	name=$(echo $dir | sed "s|[0-9A-Za-z/\-\_.\!@#$%^&*()+=]*/||g")
+	echo "$dir:$name" >> $gitRepos
 elif [ "$1" = "init" ]
 then
 	gitbaby add . $2
---
+++ gitsync.sh
@@ -0,0 +1,9 @@
+commitMessage="$(zenity --entry --text='What did you change?')"
+if [[ $? -eq 1 ]]; then
+	# cancel button pressed
+	return
+fi
+
+git add . && git commit -m "$commitMessage"
+git pull
+git push
